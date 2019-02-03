102a1a9ab0caff6d1d2aa80ce8a9b617574ea56e
Danny Ou <dannyou597@Dannys-MacBook-Pro.local>
2019-02-02 23:06:26 -0500

    developed the IBM's Watson Visual Rec. and can be used to determine link weights

--- ML.py
+++ ML.py
@@ -2,37 +2,43 @@ import json
 from watson_developer_cloud import VisualRecognitionV3
 from watson_developer_cloud import WatsonApiException
 
+
 visual_recognition = VisualRecognitionV3 (
     '2018-03-19',
-    iam_apikey = 'CRgzHfk5eC3A1H10E_P-XEGeQxXLVxPQ-zJEWrjStav1')
+    iam_apikey = 'X_euWqW0sHEleKL5MkNdFFVwidBozz05MY_KPE9Rz-An')
+
 
 try:
     # Invoke a Visual Recognition method
     classifiers = visual_recognition.list_classifiers(verbose=True).get_result()
-    print (classifiers['classifier_id'])
-    for x in classifiers:
-        print (x)
-        #visual_recognition.delete_classifier(x)
-    
+    print (json.dumps(classifiers, indent=2))
+    print ((classifiers))
+    for i in classifiers ['classifiers']:
+        x = i ['classifier_id']
+        visual_recognition.delete_classifier(x)
 except WatsonApiException as ex:
     print "Method failed with status code " + str(ex.code) + ": " + ex.message
 
 
 
-# with open('./testingData/beagle.zip', 'rb') as beagle, open(
-#         './testingData/golden-retriever.zip', 'rb') as goldenretriever, open(
-#             './testingData/husky.zip', 'rb') as husky, open(
-#                 './testingData/cats.zip', 'rb') as cats:
-#     model = visual_recognition.create_classifier(
-#         'dogs',
-#         beagle_positive_examples=beagle,
-#         goldenretriever_positive_examples=goldenretriever,
-#         husky_positive_examples=husky,
-#         negative_examples=cats).get_result()
-# print(json.dumps(model, indent=2))
-# modelID = ""
+with open('./testingData/beagle.zip', 'rb') as beagle, open(
+        './testingData/golden-retriever.zip', 'rb') as goldenretriever, open(
+            './testingData/husky.zip', 'rb') as husky, open(
+                './testingData/cats.zip', 'rb') as cats:
+    model = visual_recognition.create_classifier(
+        'dogs',
+        beagle_positive_examples=beagle,
+        goldenretriever_positive_examples=goldenretriever,
+        husky_positive_examples=husky,
+        negative_examples=cats).get_result()
+print(json.dumps(model, indent=2))
+modelID = ""
 
-# if 'classifiers_id' in model:
-#     modelID = model.get("classifier_id")
+modelID = model ['classifier_id']
 
-# print (modelID)
+with open ('./first.jpg','rb') as image_file:
+    classes = visual_recognition.classify(
+        image_file,
+        threshold= '0.5',
+        owners= ["me"],classifier_ids=modelID).get_result()
+    print (json.dumps(classes,indent =2))
