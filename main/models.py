899b91ca6b0501f6d145d11f4866c13c2a82f0e1
Kevin He <yc4he@uwaterloo.ca>
2019-02-03 04:17:58 -0500

    added image uploader

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+# #models.py
+# from django.db import models
 
-# Create your models here.
+
+# class Document(models.Model):
+#     upload_by = models.ForeignKey('auth.User', related_name='uploaded_documents', on_delete=models.PROTECT)
+#     datestamp = models.DateTimeField(auto_now_add=True)
+#     document = models.ImageField(upload_to='uploads/')
+#     # ...
+#     def __unicode__(self):
+#        return self.document
+
+
+# class MainModel(models.Model):
+#     title = models.CharField(max_length=42)
+#     document = models.ForeignKey(Document, on_delete=models.PROTECT)
+#     #...
--- main/views.py
+++ main/views.py
@@ -1,15 +1,29 @@
-from django.shortcuts import render, HttpResponse, HttpResponseRedirect
-from django.views.generic import TemplateView
-# from django.forms import NameForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from .forms import UploadFileForm
+
+
+def handle_uploaded_file(f):
+    with open('some/file/name.txt', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
 
 def index(request):
     if request.method == 'GET':
         return render(request, 'index.html')
-    # elif request.method == 'POST':
-        # form = NameForm(request.POST)
-        # if form.is_valid():
-            # return HttpResponseRedirect('/')
-
-
 # Create your views here.
+
+import os 
+
+from django.conf import settings
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from django.core.files.storage import default_storage
+
+
+def file_upload(request):
+    save_path = os.path.join(settings.MEDIA_ROOT, 'uploads', request.FILES['file'])
+    path = default_storage.save(save_path, request.FILES['file'])
+    return default_storage.path(path)
 
