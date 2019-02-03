58565df60f39083221f743a094db33da59ff79d8
blueXstar597 <dannyou597@gmail.com>
2019-02-03 03:29:25 -0500

    Using IBM Watson + Custom Model to specify style from the user

--- ML.py
+++ ML.py
@@ -1,7 +1,7 @@
 import json
 from watson_developer_cloud import VisualRecognitionV3
 from watson_developer_cloud import WatsonApiException
-
+import numpy
 
 visual_recognition = VisualRecognitionV3 (
     '2018-03-19',
@@ -23,3 +23,24 @@ try:
             print (json.dumps(classes,indent =2))
 except WatsonApiException as ex:
     print "Method failed with status code " + str(ex.code) + ": " + ex.message
+
+wearDesign = {}
+for i in classes ['images']:
+    for x in i ['classifiers']:
+        for y in x ['classes']:
+            wearDesign [y['class']] = y['score']
+
+specialSelection = [];
+with open ('./first.jpg', 'rb') as image_file:
+    classes_result = visual_recognition.classify(image_file).get_result()
+    for i in classes_result ['images']:
+        for x in i ['classifiers']:
+            for y in x ['classes']:
+                #print (y)
+                if y['score']>=0.6:
+                    specialSelection.append(y['class'])
+#print(json.dumps(classes_result, indent=2))
+
+
+print (specialSelection)
+print (wearDesign)
